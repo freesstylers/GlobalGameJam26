646ee368cc3345c510a7c15408347a30816bc685
GCidon <cidonchap@gmail.com>
2026-01-30 19:12:44 +0100

    Enemigo base

---
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour
+{
+
+
+    void Awake()
+    {
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
