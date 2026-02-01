4f434cc5ea686ab277c01361718e1e898eeb0b84
Jose Martin Serrano <josemsdev@gmail.com>
2026-02-01 01:55:12 +0100

    Efectos de las balas con cambio de color al vuelo

--- Assets/Scripts/Bullets/PlayerBullet.cs
+++ Assets/Scripts/Bullets/PlayerBullet.cs
@@ -1,9 +1,12 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class PlayerBullet : BasicBullet
 {
     public Color[] particleColors = { Color.red, Color.green, Color.blue };
+
+    private VisualEffect vfx;
     
     private void Awake()
     {
@@ -13,7 +16,9 @@ public class PlayerBullet : BasicBullet
     private void Start()
     {
         Mask currentMask = FlowManager.instance.GetCurrentMask();
-
+        vfx = GetComponent<VisualEffect>();
+        vfx.SetVector4("MaskColor", particleColors[(int)currentMask.color_]);
+        vfx.SetVector3("Direction", -dir);
         //Setting stats
         //speed = currentMask.stats_.baseBulletSpeed_;
 
@@ -23,7 +28,7 @@ public class PlayerBullet : BasicBullet
 
     public void OnMaskChange(Mask newMask)
     {
-
+        vfx.SetVector4("MaskColor", particleColors[(int)newMask.color_]);
         //Le asignamos a lo que necesite el color
         color = particleColors[((int)newMask.color_)];
     }
